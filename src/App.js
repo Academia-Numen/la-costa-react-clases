9d16cb1dcdc33f64660d822b7d8830983355d038
Aaron Ivan Amaro <aaronivanamaro@gmail.com>
2022-05-16 20:25:31 -0300

    Avances hasta props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Lista from './components/Lista';
 
 function App() {
 
@@ -7,30 +8,34 @@ function App() {
     tema: 'Angular'
   }
 
-  const autos = [
-    { id: 1, nombre: 'Corsa', marca: 'Chevrolet' },
-    { id: 2, nombre: 'Siena', marca: 'Fiat' },
-    { id: 3, nombre: 'Ka', marca: 'Ford' },
-    { id: 4, nombre: 'Gol', marca: 'Volkswagen' },
-    { id: 5, nombre: 'A7', marca: 'Audi' },
-  ]
-
   const estilos = {
     color: 'blue',
     fontSize: 30,
   }
 
+  const autos = [
+    { id: 1, strong: 'Corsa', span: 'Chevrolet' },
+    { id: 2, strong: 'Siena', span: 'Fiat' },
+    { id: 3, strong: 'Ka', span: 'Ford' },
+    { id: 4, strong: 'Gol', span: 'Volkswagen' },
+    { id: 5, strong: 'A7', span: 'Audi' },
+  ]
+
+  const electrodomesticos = [
+    { id: 1, strong: 'Heladera', span: 'Gafa' },
+    { id: 2, strong: 'Secarropas', span: 'Kohinoor' },
+    { id: 3, strong: 'Cocina', span: 'Escorial' },
+    { id: 4, strong: 'Batidora', span: 'Peabod' },
+    { id: 5, strong: 'Aire Acondicionado', span: 'Lift' },
+  ]
+
   return (
     <div className="App">
       <h2 style={estilos}>Bienvenidos a las clase {clase.numero} de {clase.tema} </h2>
-      <ul>
-        {autos.map(auto => (
-          <li key={auto.id}>
-            <strong>{auto.marca}</strong>
-            <span>{auto.nombre}</span>
-          </li>
-        ))}
-      </ul>
+      <Lista lista={autos} titulo='Lista de Autos' />
+      <Lista lista={electrodomesticos} titulo='Lista de Electrodomésticos'>
+        <li style={{ backgroundColor: 'red', color: 'white' }}>Fin de la lista</li>
+      </Lista>
     </div>
   );
 }
