581ee91dedfc9a505eb13b3211791bd6a1892452
Aaron Ivan Amaro <aaronivanamaro@gmail.com>
2022-05-23 20:27:07 -0300

    Clase 3: ejemplos de useState

--- src/App.js
+++ src/App.js
@@ -1,36 +1,51 @@
+import { useState } from 'react';
 import './App.css';
+import Contador from './components/Contador';
+import Lista from './components/Lista';
+
+const autosData = [
+  { id: 1, strong: 'Corsa', span: 'Chevrolet' },
+  { id: 2, strong: 'Siena', span: 'Fiat' },
+  { id: 3, strong: 'Ka', span: 'Ford' },
+  { id: 4, strong: 'Gol', span: 'Volkswagen' },
+  { id: 5, strong: 'A7', span: 'Audi' },
+]
+
+const electrodomesticosData = [
+  { id: 1, strong: 'Heladera', span: 'Gafa' },
+  { id: 2, strong: 'Secarropas', span: 'Kohinoor' },
+  { id: 3, strong: 'Cocina', span: 'Escorial' },
+  { id: 4, strong: 'Batidora', span: 'Peabod' },
+  { id: 5, strong: 'Aire Acondicionado', span: 'Lift' },
+]
 
 function App() {
 
-  const estilos = {
-    div1: { backgroundColor: 'red', width: 650, height: 400, margin: '0 auto' },
-    div2: { backgroundColor: 'yellow', width: 450, height: 300, margin: '0 auto' },
-    div3: { backgroundColor: 'green', width: 300, height: 200, margin: '0 auto', 
-      display: 'flex', justifyContent: 'center', alignItems: 'center' },
-    btn: { height: 50 },
-  }
 
-  function eventHandler(e) {
-    e.target.style.backgroundColor = 'blue';
-  }
+  const [estado, setEstado] = useState('Valor Inicial del Estado');
+  const [autos, setAutos] = useState(() => autosData);
+  const [electrodomesticos, setElectrodomesticos] = useState(electrodomesticosData);
 
-  function detenerPropagacion(e) {
-    e.stopPropagation();
-  }
+  function actualizarEstado() {
+    setEstado('Nuevo valor de Estado');
+    setAutos(prevState => [...prevState, 
+      { id: 6, strong: 'Bora', span: 'Volkswagen' },
+      { id: 7, strong: 'TT', span: 'Audi' },
+    ])
+    setElectrodomesticos(prevState => [...prevState, 
+      { id: 6, strong: 'Aire Acondicionado', span: 'LG' },
+    ])
+  };
 
   return (
-    <div style={estilos.div1} onClick={eventHandler}>
+    <div>
+      <Contador />
 
-      <div style={estilos.div2} onClick={detenerPropagacion}> 
+      <button onClick={actualizarEstado}>Actualizar Estado</button>
+      <h1>{ estado }</h1>
 
-        <div style={estilos.div3}>
-          
-          <button style={estilos.btn}>Presioname</button>
-        
-        </div>
-      
-      </div>
-
+      <Lista titulo='Lista de Autos' lista={autos} />
+      <Lista titulo='Lista de Electrodomesticos' lista={electrodomesticos} />
     </div>
   );
 }
