76ae63ef6cd0d4e0319581a174c21bd59cea4fba
Aaron Ivan Amaro <aaronivanamaro@gmail.com>
2022-05-30 21:06:58 -0300

    Clase 4: Ejemplo useEffect con json-placeholder y json-server

---
+++ src/components/Lista.js
@@ -0,0 +1,111 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Lista = () => {
+
+    const [data, setData] = useState([]);
+    const [error, setError] = useState({
+        isError: false,
+        message: '',
+    });
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [worldCupData, setWorldCupData] = useState([]);
+    const [worldCupError, setWorldCupError] = useState({
+        isError: false,
+        message: '',
+    });
+    const [isWorldCupLoading, setIsWorldCupLoading] = useState(false);
+
+    const [botonActualizador, setBotonActualizador] = useState(false);
+
+    useEffect(() => {
+        setIsWorldCupLoading(true);
+        axios('http://localhost:8000/team')
+            .then(res => setWorldCupData(res.data))
+            .catch(err => setWorldCupError({
+                isError: true,
+                message: err.message,
+            }))
+            .finally(() => {
+                setTimeout(() => {
+                    setIsWorldCupLoading(false)
+                }, 1000)
+            })
+    }, [botonActualizador])
+
+    useEffect(() => {
+        setIsLoading(true);
+        axios('https://jsonplaceholder.typicode.com/users')
+            .then(res => setData(res.data))
+            .catch(err => setError({
+                isError: true,
+                message: err.message,
+            }))
+            .finally(() => {
+                setTimeout(() => {
+                    setIsLoading(false)
+                }, 1000)
+            })
+    }, [botonActualizador])
+
+    return (
+        <>
+            <button onClick={() => setBotonActualizador(!botonActualizador)}>Boton Actualizador</button>
+
+            <ul>
+                {
+                    error.isError ? <h4 style={{ color: 'red' }}>Error: {error.message}</h4> :
+
+                        isLoading ? <h4>Cargando....</h4> :
+
+                            data.length !== 0 &&
+
+                            data.map(item => (
+                                <li>{item.name} --- {item.email}</li>
+                            ))
+                }
+            </ul>
+
+            <ul style={{
+                display: 'flex',
+                gap: '1rem',
+                listStyle: 'none',
+            }}>
+                {
+                    worldCupError.isError ? <h4 style={{ color: 'red' }}>Error: {worldCupError.message}</h4> :
+
+                        isWorldCupLoading ? <h4>Cargando....</h4> :
+
+                            worldCupData.length !== 0 &&
+      
+                            worldCupData.matches.map(({ match, date, day, time, rival, stadium, img}) => (
+                                <li key={match}>
+                                    <div style={{
+                                        maxWidth: '200px',
+                                        background: 'skyblue',
+                                        padding: 10,
+
+                                    }}>
+                                        <h2>{rival}</h2>
+                                        <img src={img} alt={rival} style={{ maxWidth: '100%'}}/>
+                                        <div style={{
+                                            display: 'flex',
+                                            gap: '1rem',
+                                            fontSize: 13,
+                                        }}>
+                                            <p>{date}</p>
+                                            <p>{day}</p>
+                                            <p>{time}</p>
+                                        </div>
+                                        <h3>{stadium}</h3>
+                                    </div>
+                                </li>
+                            ))
+                }
+            </ul>
+        </>
+    );
+}
+
+export default Lista;
