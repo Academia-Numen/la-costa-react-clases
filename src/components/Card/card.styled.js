a3bd8fba6da4e970ca462c1499445cb02cfd3859
Aaron Ivan Amaro <aaronivanamaro@gmail.com>
2022-06-23 20:10:54 -0300

    clase 6: hicimos andar el sector de hijo

--- src/components/Card/card.styled.js
+++ src/components/Card/card.styled.js
@@ -1,4 +1,4 @@
-import styled from "@emotion/styled";
+import styled from "@emotion/styled/macro";
 
 export const CardContainer = styled.li`
     max-width: 200px;
--- src/components/Lista/lista.styled.js
+++ src/components/Lista/lista.styled.js
@@ -1,8 +1,14 @@
-import styled from "@emotion/styled";
-import { CardContainer } from "../Card/card.styled";
+import styled from "@emotion/styled/macro";
+import { CardContainer, Text } from "../Card/card.styled";
 
 export const CardsContainer = styled.ul`
     display: flex;
     gap: 1rem;
     list-style: none;
+    ${CardContainer}:first-of-type {
+        color: red;
+        ${Text} {
+            background: red;
+        }
+    }
 `;
