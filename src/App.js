671f6c64a77ea46ef0009d92b254bbd3d0770835
Aaron Ivan Amaro <aaronivanamaro@gmail.com>
2022-06-23 19:51:45 -0300

    clase 6 parte B: avances antes del corte

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
-import Lista from './components/Lista';
+import BooksList from './components/BooksList';
 import React from 'react';
+import { useState } from 'react';
+import { createContext } from 'react';
+
+const BooksContext = createContext();
 
 function App() {
+  const [books, setBooks] = useState([
+    { title: '100 años de soledad', id: 1 },
+    { title: 'las venas abiertas de america latina', id: 2 },
+    { title: 'El viejo y el mar', id: 3 },
+    { title: 'Sandokan', id: 4 },
+  ])
 
   return (
     <div>
-      <Lista/>
+      <BooksContext.Provider value={books}>
+        <div>
+          <div>
+            <BooksList/>
+          </div>
+        </div>
+      </BooksContext.Provider>
     </div>
   );
 }
---
+++ src/components/BooksList/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BooksList = () => {
+
+    return (
+        <div>
+            Hola
+        </div>
+    );
+}
+
+export default BooksList;
